86f23d493a370df4f13b0d50f048e82db0dfd0b1
Torai Lin <torailin55@gmail.com>
2023-10-23 19:55:53 +0800

    update: Unit4 Machine Language

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,105 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// screen: 256 * 512 pixel
+// register: 256 * 512 / 16 = 8192
+// SCREEN[0] ~ SCREEN[8191] need to be black
+
+// pseudo code:
+
+// kbd = 0
+// scn_addr = SCREEN
+// scn_length = 8192
+// i = 0
+
+// while (true) {
+//   if (kbd === KBD[0]) {
+//     continue
+//   }
+//   kbd = KBD[0]
+// 
+//   for (i=0; i<scn_length; i++) {
+//     if (kbd !== 0) {
+//       scn_addr[i] = -1 // (∵ 16-bit 2'complement -1 = 1111 1111 1111 1111)
+//     } else {
+//       scn_addr[i] = 0
+//     }
+//   }
+// }
+
+@kbd
+M=0
+
+@SCREEN
+D=A
+@scn_addr
+M=D
+
+@8192
+D=A
+@scn_length
+M=D
+
+@i
+M=0
+
+(INPUT_LOOP)
+  @kbd
+  D=M
+  @KBD
+  D=D-M
+  @INPUT_LOOP
+  D;JEQ
+
+  @KBD
+  D=M
+  @kbd
+  M=D
+
+  @i
+  M=0
+
+  (SCREEN_LOOP)
+    @i
+    D=M
+    @scn_length
+    D=M-D
+    @INPUT_LOOP
+    D;JLE
+
+    @kbd
+    D=M
+    @SET_SCREEN_BLACK
+    D;JNE
+
+    @SET_SCREEN_WHITE
+    0;JMP
+
+    (SET_SCREEN_BLACK)
+      @i
+      D=M
+      @scn_addr
+      A=M+D
+      M=-1
+      
+      @i
+      M=M+1
+
+      @SCREEN_LOOP
+      0;JMP
+    
+    (SET_SCREEN_WHITE)
+      @i
+      D=M
+      @scn_addr
+      A=M+D
+      M=0
+      
+      @i
+      M=M+1
+
+      @SCREEN_LOOP
+      0;JMP
+
+  @INPUT_LOOP
+  0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,60 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// pseudo code:
+
+// result = 0
+// multiplicand = RAM[0]
+// multiplier = RAM[1]
+// i = 0
+
+// for (i=0; i<multiplier; i++) {
+//   result+=multiplicand
+// }
+// RAM[2] = result
+
+  @result
+  M=0
+
+  @R0
+  D=M
+  @multiplicand
+  M=D
+
+  @R1
+  D=M
+  @multiplier
+  M=D
+
+  @i
+  M=0
+
+(LOOP)
+  @i
+  D=M
+  @multiplier
+  D=D-M
+  @OUT
+  D;JGE
+
+  @multiplicand
+  D=M
+  @result
+  M=D+M
+
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+
+(OUT)
+  @result
+  D=M
+  @R2
+  M=D
+  @END
+  0;JMP
+
+(END)
+  @END
+  0;JMP
